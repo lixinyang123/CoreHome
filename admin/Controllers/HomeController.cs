a2b51ad13c4a578b7cf777f93d7fcc2a10fec9d1
李新阳 <lixinyangemil@outlook.com>
2019-08-26 22:23:47 +0800

    添加管理员项目

---
+++ admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using DataContext.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
