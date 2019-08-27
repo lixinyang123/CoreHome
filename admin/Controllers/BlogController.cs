500bf8f0e7342bfbb6291fd6421f4d8bccaf9bbb
李新阳 <lixinyangemil@outlook.com>
2019-08-27 12:26:19 +0800

    基础搭建

---
+++ admin/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using DataContext.DbOperator;
+using DataContext.Models;
+using Infrastructure.common;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace admin.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ArticleRepository articleRepository;
+        private readonly int pageSize = 20;
+
+        public BlogController()
+        {
+            articleRepository = new ArticleRepository();
+        }
+
+        public IActionResult Index(int index)
+        {
+            index = PageManager.GetStartIndex(index, articleRepository.Count(), pageSize);
+            List<Article> articles = articleRepository.Find(index, pageSize);
+            return View(articles);
+        }
+
+        public IActionResult DelArticle(int id)
+        {
+            return Redirect("Index");
+        }
+    }
+}
--- admin/Controllers/HomeController.cs
+++ admin/Controllers/HomeController.cs
@@ -1,27 +1,14 @@
-﻿using DataContext.DbOperator;
-using DataContext.Models;
-using Infrastructure.common;
+﻿using DataContext.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace admin.Controllers
 {
     public class HomeController : Controller
     {
-        private ArticleRepository articleRepository;
-        private int pageSize = 20;
-
-        public HomeController()
-        {
-            articleRepository = new ArticleRepository();
-        }
-
         public IActionResult Index(int index)
         {
-            index = PageManager.GetStartIndex(index, articleRepository.Count(), pageSize);
-            List<Article> articles = articleRepository.Find(index,pageSize);
-            return View(articles);
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
