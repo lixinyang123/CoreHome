6216bc1b8f07b17855ab4f0f533c4b61e6067cd8
李新阳 <lixinyangemil@outlook.com>
2019-08-28 12:40:47 +0800

    登录界面完善

--- admin/Controllers/HomeController.cs
+++ admin/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
-﻿using DataContext.Models;
+﻿using admin.Models;
+using DataContext.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace admin.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            string url = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1";
+            string jsonStr = await new HttpClient().GetStringAsync(url);
+            BingWallpaper wallpaper = JsonConvert.DeserializeObject<BingWallpaper>(jsonStr);
+            ViewBag.picUrl = "http://cn.bing.com" + wallpaper.images[0].url;
             return View();
         }
 
---
+++ admin/Models/BingWallpaper.cs
@@ -0,0 +1,37 @@
+﻿namespace admin.Models
+{
+    public class BingWallpaper
+    {
+        public Image[] images { get; set; }
+        public Tooltips tooltips { get; set; }
+    }
+
+    public class Tooltips
+    {
+        public string loading { get; set; }
+        public string previous { get; set; }
+        public string next { get; set; }
+        public string walle { get; set; }
+        public string walls { get; set; }
+    }
+
+    public class Image
+    {
+        public string startdate { get; set; }
+        public string fullstartdate { get; set; }
+        public string enddate { get; set; }
+        public string url { get; set; }
+        public string urlbase { get; set; }
+        public string copyright { get; set; }
+        public string copyrightlink { get; set; }
+        public string title { get; set; }
+        public string quiz { get; set; }
+        public bool wp { get; set; }
+        public string hsh { get; set; }
+        public int drk { get; set; }
+        public int top { get; set; }
+        public int bot { get; set; }
+        public object[] hs { get; set; }
+    }
+
+}
