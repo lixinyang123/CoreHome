1978516bebb0fbddc82a5ec9b8bddd97bc8b1799
李新阳 <lixinyangemil@outlook.com>
2019-08-28 18:50:26 +0800

    新增登录验证功能

--- admin/Controllers/BlogController.cs
+++ admin/Controllers/BlogController.cs
@@ -1,20 +1,46 @@
-﻿using DataContext.DbOperator;
+﻿using System;
+using System.Collections.Generic;
+using DataContext.DbOperator;
 using DataContext.Models;
 using Infrastructure.common;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace admin.Controllers
 {
     public class BlogController : Controller
     {
         private readonly ArticleRepository articleRepository;
+        private IMemoryCache cache;
         private readonly int pageSize = 20;
 
-        public BlogController()
+        public BlogController(IMemoryCache _cache)
         {
+            cache = _cache;
             articleRepository = new ArticleRepository();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            try
+            {
+                string admin = Request.Cookies["admin"];
+                ISession session = HttpContext.Session;
+                string sessionStr = session.GetString(admin);
+                string cacheStr = cache.Get<string>(admin);
+                if (sessionStr != cacheStr)
+                {
+                    context.HttpContext.Response.Redirect("/Home");
+                }
+            }
+            catch (Exception)
+            {
+                context.HttpContext.Response.Redirect("/Home");
+            }
+
+            base.OnActionExecuting(context);
         }
 
         public IActionResult Index(int index)
--- admin/Controllers/OverviewController.cs
+++ admin/Controllers/OverviewController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace admin.Controllers
@@ -13,14 +14,32 @@ namespace admin.Controllers
         //验证方式
         //通过cookie.Get("admin")值获取session和cache中的accessToken进行对比
 
-        public OverviewController(IMemoryCache cache)
+        private IMemoryCache cache;
+
+        public OverviewController(IMemoryCache _cache)
         {
-            //string admin = Request.Cookies["admin"];
-            //ISession session = HttpContext.Session;
-            //if(session.GetString(admin) != cache.Get<string>(admin))
-            //{
-            //    Response.Redirect("/Home");
-            //}
+            cache = _cache;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            try
+            {
+                string admin = Request.Cookies["admin"];
+                ISession session = HttpContext.Session;
+                string sessionStr = session.GetString(admin);
+                string cacheStr = cache.Get<string>(admin);
+                if (sessionStr != cacheStr)
+                {
+                    context.HttpContext.Response.Redirect("/Home");
+                }
+            }
+            catch (Exception)
+            {
+                context.HttpContext.Response.Redirect("/Home");
+            }
+
+            base.OnActionExecuting(context);
         }
 
         public IActionResult Index()
