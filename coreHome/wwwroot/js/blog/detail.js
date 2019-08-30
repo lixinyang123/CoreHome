95d7ef6aec1f52469098482a43656777e8fb422e
李新阳 <lixinyangemil@outlook.com>
2019-08-30 23:50:11 +0800

    html与js分离

---
+++ coreHome/wwwroot/js/blog/detail.js
@@ -0,0 +1,7 @@
+﻿function initPage() {
+    var md = document.getElementById("md").value;
+    document.getElementById('detail').innerHTML = marked(md);
+
+    hljs.initHighlighting();
+    hljs.initLineNumbersOnLoad();
+}
