8046b3f183e0b4b67b457f79894062461976fcaa
李新阳 <lixinyangemil@outlook.com>
2019-11-13 18:51:26 +0800

    管理员主题管理

---
+++ admin/Controllers/ThemeController.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.common;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace admin.Controllers
+{
+    public class ThemeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Theme theme = ThemeManager.GetTheme();
+            return View(theme);
+        }
+    }
+}
