b0c6385facf8ffc424a02f2db7c5d94745c73f80
李新阳 <lixinyangemil@outlook.com>
2019-11-16 01:04:48 +0800

    修复反馈不需要验证码的bug

--- coreHome/Controllers/FeedBackController.cs
+++ coreHome/Controllers/FeedBackController.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace coreHome.Controllers
@@ -11,13 +12,25 @@ namespace coreHome.Controllers
 
             if (Request.Method == "POST")
             {
-                string contact = Request.Form["contact"];
-                string title = Request.Form["title"];
-                string detail = Request.Form["detail"];
+                ISession session = HttpContext.Session;
+                string str = session.GetString("Verification");
+                string code = Request.Form["code"];
+                if (str == null)
+                {
+                    return Redirect("/Home/Message?msg=请先同意隐私策略&url=/Feedback");
+                }
 
-                NotifyService.PushNotify(title + $"[{contact}]", detail);
+                if (code != null && code != string.Empty && str == code.ToLower())
+                {
+                    string contact = Request.Form["contact"];
+                    string title = Request.Form["title"];
+                    string detail = Request.Form["detail"];
 
-                return Redirect("/Home/Message?msg=感谢您的反馈，开发者会尽快答复&url=/Feedback");
+                    NotifyService.PushNotify(title + $"[{contact}]", detail);
+
+                    return Redirect("/Home/Message?msg=感谢您的反馈，开发者会尽快答复&url=/Feedback");
+                }
+                return Redirect("/Home/Message?msg=验证码错误&url=/Feedback");
             }
             return View();
         }
