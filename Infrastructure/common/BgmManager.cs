47eb99c91882285d7ffd4961b0f4c2b3002884c7
李新阳 <lixinyangemil@outlook.com>
2019-11-22 00:26:57 +0800

    新增展示播放列表

---
+++ Infrastructure/common/BgmManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Infrastructure.common
+{
+    public static class BgmManager
+    {
+        public static readonly string bgmPath = @"C:/Server/coreHome/Bgm/";
+
+        public static List<string> GetBgmList()
+        {
+            string[] paths = Directory.GetFiles(bgmPath);
+            List<string> musics = new List<string>();
+            foreach (string path in paths)
+            {
+                FileInfo fileInfo = new FileInfo(path);
+                musics.Add(fileInfo.Name);
+            }
+            return musics;
+        }
+    }
+}
---
+++ admin/Controllers/BgmController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure.common;
+
+namespace admin.Controllers
+{
+    public class BgmController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> musics = BgmManager.GetBgmList();
+            return View(musics);
+        }
+
+        [HttpPost]
+        public IActionResult UploadMusic(IFormFile music)
+        {
+            return RedirectToAction("index");
+        }
+
+    }
+}
