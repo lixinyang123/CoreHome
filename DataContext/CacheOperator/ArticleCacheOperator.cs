075cad2223d593c2a4539dcc7e2fda9beed64b22
李新阳 <lixinyangemil@outlook.com>
2019-11-29 01:54:00 +0800

    完善注释，添加删除缓存功能

--- DataContext/CacheOperator/ArticleCacheOperator.cs
+++ DataContext/CacheOperator/ArticleCacheOperator.cs
@@ -16,12 +16,22 @@ namespace DataContext.CacheOperator
             database = new DbConfigurator().CreateArticleCacheContext();
         }
 
+        /// <summary>
+        /// 添加缓存
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="model">value</param>
         public void AddModel(string key, Article model)
         {
             string jsonStr = JsonSerializer.Serialize(model);
             database.StringSet(key, jsonStr);
         }
 
+        /// <summary>
+        /// 添加缓存列表
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <param name="list">value</param>
         public void AddList(string key, List<Article> list)
         {
             list.ForEach(i =>
@@ -31,12 +41,22 @@ namespace DataContext.CacheOperator
             });
         }
 
+        /// <summary>
+        /// 获取缓存
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>value</returns>
         public Article GetModel(string key)
         {
             string jsonStr = database.StringGet(key);
             return JsonSerializer.Deserialize<Article>(jsonStr);
         }
 
+        /// <summary>
+        /// 读取缓存列表
+        /// </summary>
+        /// <param name="key">key</param>
+        /// <returns>value</returns>
         public List<Article> GetList(string key)
         {
             RedisValue[] values = database.ListRange(key);
@@ -47,6 +67,15 @@ namespace DataContext.CacheOperator
                 articles.Add(JsonSerializer.Deserialize<Article>(i));
             });
             return articles.OrderByDescending(i => i.ID).ToList();
+        }
+
+        /// <summary>
+        /// 删除缓存
+        /// </summary>
+        /// <param name="key">key</param>
+        public void DelKey(string key)
+        {
+            database.KeyDelete(key);
         }
     }
 }
