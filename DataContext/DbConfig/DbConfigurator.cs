3b3fd6aa140df9c29cc285980d32ac411ab8b69f
李新阳 <lixinyangemil@outlook.com>
2019-12-16 08:16:47 +0800

    dbContext增加线程安全

--- DataContext/DbConfig/DbConfigurator.cs
+++ DataContext/DbConfig/DbConfigurator.cs
@@ -9,6 +9,7 @@ namespace DataContext.DbConfig
         private const string articleConnection = "server=localhost;user id=root;password=lxy15937905153;database=articles";
         private const string redisConnection = "localhost";
 
+        private static readonly object dbLock = new object();
         private static ArticleDbContext context = null;
 
         private DbConfigurator() { }
@@ -20,14 +21,17 @@ namespace DataContext.DbConfig
 
         public static ArticleDbContext CreateArticleDbContext()
         {
-            if (context == null)
+            lock (dbLock)
             {
-                DbContextOptionsBuilder<ArticleDbContext> optionBuilder = new DbContextOptionsBuilder<ArticleDbContext>();
-                optionBuilder.UseMySQL(articleConnection);
-                context = new ArticleDbContext(optionBuilder.Options);
-                context.Database.EnsureCreatedAsync();
+                if (context == null)
+                {
+                    DbContextOptionsBuilder<ArticleDbContext> optionBuilder = new DbContextOptionsBuilder<ArticleDbContext>();
+                    optionBuilder.UseMySQL(articleConnection);
+                    context = new ArticleDbContext(optionBuilder.Options);
+                    context.Database.EnsureCreatedAsync();
+                }
+                return context;
             }
-            return context;
         }
 
         public static ConnectionMultiplexer CreateCacheContext()
