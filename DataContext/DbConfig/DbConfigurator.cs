474ffb2f3a9529357bffa84f54786a735e71d5ae
李新阳 <lixinyangemil@outlook.com>
2019-12-16 08:25:01 +0800

    取消数据库上下文线程锁

--- DataContext/DbConfig/DbConfigurator.cs
+++ DataContext/DbConfig/DbConfigurator.cs
@@ -9,7 +9,6 @@ namespace DataContext.DbConfig
         private const string articleConnection = "server=localhost;user id=root;password=lxy15937905153;database=articles";
         private const string redisConnection = "localhost";
 
-        private static readonly object dbLock = new object();
         private static ArticleDbContext context = null;
 
         private DbConfigurator() { }
@@ -21,17 +20,14 @@ namespace DataContext.DbConfig
 
         public static ArticleDbContext CreateArticleDbContext()
         {
-            lock (dbLock)
+            if (context == null)
             {
-                if (context == null)
-                {
-                    DbContextOptionsBuilder<ArticleDbContext> optionBuilder = new DbContextOptionsBuilder<ArticleDbContext>();
-                    optionBuilder.UseMySQL(articleConnection);
-                    context = new ArticleDbContext(optionBuilder.Options);
-                    context.Database.EnsureCreatedAsync();
-                }
-                return context;
+                DbContextOptionsBuilder<ArticleDbContext> optionBuilder = new DbContextOptionsBuilder<ArticleDbContext>();
+                optionBuilder.UseMySQL(articleConnection);
+                context = new ArticleDbContext(optionBuilder.Options);
+                context.Database.EnsureCreatedAsync();
             }
+            return context;
         }
 
         public static ConnectionMultiplexer CreateCacheContext()
