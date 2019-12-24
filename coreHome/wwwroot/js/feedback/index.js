59e6c804c001e7b2f6f23f5234900ed298c1af39
李新阳 <lixinyangemil@outlook.com>
2019-12-24 20:51:48 +0800

    完善反馈中心提示

--- coreHome/wwwroot/js/feedback/index.js
+++ coreHome/wwwroot/js/feedback/index.js
@@ -21,8 +21,8 @@
         success: function () {
             window.location.href = "/Home/Message?msg=感谢您的反馈，开发者会尽快答复&url=/Feedback";
         },
-        error: function () {
-            alert("验证码错误");
+        error: function (err) {
+            alert(JSON.parse(err.responseText).detail);
             document.getElementById("verfyImg").click();
         }
     });
