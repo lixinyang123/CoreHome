fb52e6961f66a6bdda933827b09f1e91de0f2509
李新阳 <lixinyangemil@outlook.com>
2020-02-19 13:07:51 +0800

    优化代码

--- CoreHome.HomePage/wwwroot/js/home/index.js
+++ CoreHome.HomePage/wwwroot/js/home/index.js
@@ -20,52 +20,15 @@ function moveToDC() {
     }
 }
 
-//===============RotateLogo================
-
-var angle = 0;
-
-function Rotate() {
-    var logo = document.getElementById("profile-thumb");
-    logo.style.transform = "rotate(" + (angle) + "deg)";
-    angle += 0.2;
-    setTimeout(Rotate, 0.1);
-}
-
-//=============loadAnimation================
-var speed = 25;
-
-function LoadAnimation() {
-    var current = document.scrollingElement.scrollTop;
-    current += speed;
-    speed--
-    document.scrollingElement.scrollTop = current;
-    timer = setTimeout(LoadAnimation, 0.5);
-    if (document.scrollingElement.scrollTop == 0) {
-        clearTimeout(timer);
-    }
-}
-
-function CookieExist(cookieName) {
-    if (document.cookie.indexOf(cookieName) != -1) {
-        return true;
-    }
-    return false;
-}
-
 //=============Initializa================
-if (!CookieExist("CookiePolicy")) {
-    setTimeout(LoadAnimation, 1000);
-}
-
-player.addEventListener('ended', function () {
-    setTimeout(() => {
-        document.getElementById("player").play();
-    }, 1000);
-});
 
 function init() {
+    player.addEventListener('ended', function () {
+        setTimeout(() => {
+            document.getElementById("player").play();
+        }, 1000);
+    });
     addLinkTarget();
-	Rotate();
 }
 
 init();
--- CoreHome.HomePage/wwwroot/js/shared/_layout.js
+++ CoreHome.HomePage/wwwroot/js/shared/_layout.js
@@ -34,18 +34,10 @@ fullHeight();
 
 function MoveTop()
 {
-	var current = document.scrollingElement.scrollTop;
-	current-=30;
-	document.scrollingElement.scrollTop = current;
-	timer = setTimeout(MoveTop,0.1);
-	if(document.scrollingElement.scrollTop==0)
-	{
-		clearTimeout(timer);
-	}
+	window.scrollTo(0, 0);
 }
 
 $(window).scroll(function () {
-
     var $win = $(window);
     if ($win.scrollTop() > 200) {
         $('.js-top').addClass('active');
