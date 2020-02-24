cdd670203e7a9aad2f30375a6882ac28108b52d5
李新阳 <lixinyangemil@outlook.com>
2020-02-24 12:38:02 +0800

    完善代码

--- CoreHome.HomePage/wwwroot/js/home/index.js
+++ CoreHome.HomePage/wwwroot/js/home/index.js
@@ -29,7 +29,7 @@ function playMusic() {
             }, 1000);
         });
         document.getElementById("player").play();
-    } catch () {}
+    } catch (e) {}
 }
 
 //=============Initializa================
