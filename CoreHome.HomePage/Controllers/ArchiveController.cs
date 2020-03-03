9fc7ae5508fd96ea96f6ec5829e881b161833929
李新阳 <lixinyangemil@outlook.com>
2020-03-03 23:41:53 +0800

    完善归档功能

--- CoreHome.HomePage/Controllers/ArchiveController.cs
+++ CoreHome.HomePage/Controllers/ArchiveController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreHome.Data.DatabaseContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+using CoreHome.Data.Model;
 
 namespace CoreHome.HomePage.Controllers
 {
     public class ArchiveController : Controller
     {
+        private readonly ArticleDbContext articleDbContext;
+
+        public ArchiveController(ArticleDbContext articleDbContext)
+        {
+            this.articleDbContext = articleDbContext;
+        }
+        
         public IActionResult Index()
         {
+            List<Year> years = articleDbContext.Years
+                .Include(i => i.Months)
+                .ThenInclude(i => i.Articles)
+                .ToList();
+
             return View();
         }
     }
