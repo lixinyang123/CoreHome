722264b8f5f0722409497af15a78f6d8864fa4e5
lixinyang123 <lixinyangemil@outlook.com>
2020-08-13 14:26:14 +0800

    完善代码

--- CoreHome.Infrastructure/Models/OssConfig.cs
+++ CoreHome.Infrastructure/Models/OssConfig.cs
@@ -2,14 +2,14 @@
 {
     public class OssConfig
     {
-        public string EndPoint { get; set; }
-
         public string AccessKeyId { get; set; }
 
         public string AccessKeySecret { get; set; }
 
-        public string BucketName { get; set; }
+        public string EndPoint { get; set; }
 
         public string BucketDomainName { get; set; }
+
+        public string BucketName { get; set; }
     }
 }
