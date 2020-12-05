34cd34cfbda3c51f636e7c7054c730cfb938c577
LLLXY <lixinyangemil@outlook.com>
2020-12-05 17:07:32 +0000

    更换反向代理服务器

--- CoreHome.ReverseProxy/Startup.cs
+++ CoreHome.ReverseProxy/Startup.cs
@@ -1,24 +1,27 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CoreHome.ReverseProxy
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // 配置服务
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddReverseProxy().LoadFromConfig(Configuration.GetSection("ReverseProxy")); 
+        }
+
+        // 配置HTTP请求管道
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,12 +31,9 @@ namespace CoreHome.ReverseProxy
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints => 
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapReverseProxy();
             });
         }
     }
