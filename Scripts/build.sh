b5a878bcf3d0597f672a235556598f0387e39569
LLLXY <lixinyangemil@outlook.com>
2020-12-06 05:55:13 +0800

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,40 +1,8 @@
 sudo apt update
 
-sudo apt install libgdiplus mysql-server nginx -y
+sudo apt install libgdiplus mysql-server -y
 
 sudo service mysql start
-sudo service nginx start
-
-nginxConfig="
-server {
-    listen        80;
-
-    location / {
-        proxy_pass         http://localhost:5000/;
-        proxy_http_version 1.1;
-        proxy_set_header   Upgrade \$http_upgrade;
-        proxy_set_header   Connection \$http_connection;
-        proxy_set_header   Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header   X-Forwarded-Proto \$scheme;
-    }
-
-    location ~* /admin {
-        proxy_pass         http://localhost:5001;
-        proxy_http_version 1.1;
-        proxy_set_header   Upgrade \$http_upgrade;
-        proxy_set_header   Connection \$http_connection;
-        proxy_set_header   Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header   X-Forwarded-Proto \$scheme;
-    }
-}
-"
-
-sudo echo "$nginxConfig" > /etc/nginx/sites-enabled/default
-sudo nginx -s reload
 
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -43,7 +11,7 @@ rm packages-microsoft-prod.deb
 sudo apt-get update; \
 	sudo apt-get install -y apt-transport-https && \
 	sudo apt-get update && \
-	sudo apt-get install -y dotnet-sdk-3.1
+	sudo apt-get install -y dotnet-sdk-5.0
 
 dotnet tool install -g Microsoft.Web.LibraryManager.Cli
 
@@ -58,3 +26,8 @@ dotnet restore
 ~/.dotnet/tools/libman restore
 dotnet build
 dotnet publish -o ../bin/CoreHome/Admin
+
+cd ../CoreHome.ReverseProxy
+dotnet restore
+dotnet build
+dotnet publish -o ../bin/CoreHome/ReverseProxy
