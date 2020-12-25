85cfc9531d9330e99800d7adb73e960acf17e122
lixinyang123 <lixinyangemil@outlook.com>
2020-12-25 18:42:53 +0800

    解决启动时 fail: Microsoft.AspNetCore.Antiforgery.DefaultAntiforgery[7]

--- CoreHome.Admin/Startup.cs
+++ CoreHome.Admin/Startup.cs
@@ -3,6 +3,7 @@ using CoreHome.Data.DatabaseContext;
 using CoreHome.Infrastructure.Models;
 using CoreHome.Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
@@ -27,6 +28,8 @@ namespace CoreHome.Admin
         // 将服务添加到容器
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDataProtection().SetApplicationName("CoreHome");
+
             services.Configure<CookieOptions>(config =>
             {
                 config.SameSite = SameSiteMode.Lax;
