141b2f226658fe5cefbdb9082312759fa7d8d38e
lixinyang123 <lixinyangemil@outlook.com>
2021-01-27 14:19:53 +0800

    新增二维码分享 修复了评论成功通知不显示内容的BUG

---
+++ CoreHome.HomePage/wwwroot/js/blog/detail.js
@@ -0,0 +1,9 @@
+﻿function showCode() {
+    document.querySelector("#qrcode").innerHTML = "";
+    new QRCode(document.querySelector("#qrcode"), window.location.href);
+}
+
+window.onload = () => {
+    let warning = document.querySelector("#warning").value;
+    if (warning != "") { alert(warning); }
+}
