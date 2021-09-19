96c8262c59cb11c60d5324289948a7280ec521b7
LLLXY <lixinyangemil@outlook.com>
2021-09-19 18:17:51 +0800

    update build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install libgdiplus -y
+
+dotnet tool install -g Microsoft.Web.LibraryManager.Cli
+dotnet tool install -g dotnet-ef
+
+cd ./CoreHome.HomePage
+dotnet restore
+~/.dotnet/tools/libman restore
+dotnet build
+dotnet publish -o ../bin/CoreHome/HomePage
+
+cd ../CoreHome.Admin
+dotnet restore
+~/.dotnet/tools/libman restore
+dotnet build
+dotnet publish -o ../bin/CoreHome/Admin
+
+cd ../CoreHome.ReverseProxy
+dotnet restore
+dotnet build
+dotnet publish -o ../bin/CoreHome/ReverseProxy
