4aad64c66e54c219100ae52fd8f86014e05ed13e
lixinyang <lixinyangemil@outlook.com>
2021-12-13 02:50:49 +0800

    归档月份排序

--- CoreHome.Data/Models/Year.cs
+++ CoreHome.Data/Models/Year.cs
@@ -7,6 +7,12 @@ namespace CoreHome.Data.Models
         [Required]
         public int Value { get; set; }
 
-        public List<Month> Months { get; set; }
+        private List<Month> months;
+
+        public List<Month> Months
+        {
+            get => months.OrderBy(i => i.Value).ToList();
+            set => months = value;
+        }
     }
 }
