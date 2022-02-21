2c713714b96453e98f802f21a5b785bc5bc648d5
lixinyang <lixinyangemil@outlook.com>
2022-02-21 13:30:00 +0800

    remove source list

--- CoreHome.ReverseProxy/Program.cs
+++ CoreHome.ReverseProxy/Program.cs
@@ -10,10 +10,10 @@ namespace CoreHome.ReverseProxy
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-.ConfigureWebHostDefaults(webBuilder =>
-{
-    webBuilder.UseStartup<Startup>();
-});
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
         }
     }
 }
