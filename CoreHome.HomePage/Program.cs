2f60ec2faf7676733cc6d9ad4a7a3697c65f851f
lixinyang <lixinyangemil@outlook.com>
2022-03-04 18:22:59 +0800

    fix migrate repeat

--- CoreHome.HomePage/Program.cs
+++ CoreHome.HomePage/Program.cs
@@ -1,14 +1,10 @@
-﻿using CoreHome.Data.DatabaseContext;
-using Microsoft.EntityFrameworkCore;
-
-namespace CoreHome.HomePage
+﻿namespace CoreHome.HomePage
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
             IHost host = CreateHostBuilder(args).Build();
-            host.Services.GetService<ArticleDbContext>().Database.Migrate();
             host.Run();
         }
 
