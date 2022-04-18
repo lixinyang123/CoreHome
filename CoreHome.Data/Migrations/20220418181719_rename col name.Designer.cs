0ebb9eb4c5ff3d2d7380ac2bdb15536d43b02a89
lixinyang <lixinyangemil@outlook.com>
2022-04-19 02:17:48 +0800

    migrate database

--- CoreHome.Data/Migrations/20220418181719_rename col name.Designer.cs
+++ CoreHome.Data/Migrations/20220418181719_rename col name.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CoreHome.Data.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoreHome.Data.Migrations
 {
     [DbContext(typeof(ArticleDbContext))]
-    partial class ArticleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220418181719_rename col name")]
+    partial class renamecolname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CoreHome.Data/Migrations/20220418181719_rename col name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreHome.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamecolname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Notifications",
+                newName: "Time");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Time",
+                table: "Notifications",
+                newName: "DateTime");
+        }
+    }
+}
