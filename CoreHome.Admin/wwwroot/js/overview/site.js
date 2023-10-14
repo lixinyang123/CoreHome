36d1118366f5b880721991201e5a6167bc312538
lixinyang123 <lixinyangemil@outlook.com>
2023-10-15 04:17:21 +0800

    update dependences

--- CoreHome.Admin/wwwroot/js/overview/site.js
+++ CoreHome.Admin/wwwroot/js/overview/site.js
@@ -47,31 +47,7 @@ let wschartData = {
 };
 
 let chartOption = {
-    responsive: true,
-    tooltips: {
-        mode: 'index',
-        intersect: false
-    },
-    hover: {
-        mode: 'nearest',
-        intersect: true
-    },
-    scales: {
-        xAxes: [{
-            display: true,
-            scaleLabel: {
-                display: true,
-                labelString: 'Time'
-            }
-        }],
-        yAxes: [{
-            display: true,
-            scaleLabel: {
-                display: true,
-                labelString: 'Value'
-            }
-        }]
-    }
+    responsive: true
 };
 
 window.myLine = new Chart(ctx, {
